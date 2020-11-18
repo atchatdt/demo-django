69dc613a85c2265a3622f3634330741830dda39c
kuro <kisuvitinh@gmail.com>
2020-11-18 11:31:22 +0700

    urls

--- accounts/models.py
+++ accounts/models.py
@@ -13,6 +13,14 @@ class Customer(models.Model):
         return self.name
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     CATEGORY = (
         ('Indoor', 'Indoor'),
@@ -23,9 +31,13 @@ class Product(models.Model):
     price = models.FloatField(null=True)
     category = models.CharField(max_length=200, null=True, choices=CATEGORY)
     # description = models.CharField(max_length=200, null=True)
-    description = RichTextField(null=True)#models.CharField(max_length=200, null=True)
+    description = models.CharField(
+        max_length=200, null=True)  # RichTextField(null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    tags = models.ManyToManyField(Tag)
 
+    def __str__(self):
+        return self.name
 
 class Order(models.Model):
     STATUS = (
@@ -34,7 +46,8 @@ class Order(models.Model):
         ('Delivered', 'Delivered'),
     )
 
-    # customer =
-    # product =
+    customer = models.ForeignKey(
+        Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
