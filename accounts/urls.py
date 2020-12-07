3e6f8b64ad949478869bc980e7b68368050a7c52
kuro <kisuvitinh@gmail.com>
2020-12-07 13:15:14 +0700

    reset password

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
-from django.contrib import admin
 from django.urls import path
+
+from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -7,6 +8,10 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('products/', views.products, name="products"),
     path('customer/<str:pk_test>/', views.customer, name="customer"),
+
+    path('user/', views.userPage, name='user'),
+
+    path('account/', views.accountSettings, name='account_setting'),
 
     path('create_order/<str:pk>', views.createOrder, name="create_order"),
     path('update_order/<str:pk>', views.updateOrder, name="update_order"),
@@ -16,8 +21,38 @@ urlpatterns = [
     path('login/', views.loginPage, name="login"),
     path('logout/', views.logoutUser, name="logout"),
 
-    path('user/', views.userPage, name='user'),
 
-    path('account/', views.accountSettings, name = 'account_setting')
+
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html"),
+         name="reset_password"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_sent.html"),
+         name="password_reset_done"),
+    path('reset_password/<uidb64>/<token>',
+         auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_form.html"), name="password_reset_confirm"),
+    path('reset_password_complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name=
+         "accounts/password_reset_done.html"), name="password_reset_complete"),
+
 
 ]
+
+'''
+1 - Submit email form                         //PasswordResetView.as_view()
+2 - Email sent success message                //PasswordResetDoneView.as_view()
+3 - Link to password Rest form in email       //PasswordResetConfirmView.as_view()
+4 - Password successfully changed message     //PasswordResetCompleteView.as_view()
+'''
+
+
+# https://docs.djangoproject.com/en/3.0/topics/auth/default/#all-authentication-views
+''' 
+accounts/login/ [name='login']
+accounts/logout/ [name='logout']
+accounts/password_change/ [name='password_change']
+accounts/password_change/done/ [name='password_change_done']
+accounts/password_reset/ [name='password_reset']
+accounts/password_reset/done/ [name='password_reset_done']
+accounts/reset/<uidb64>/<token>/ [name='password_reset_confirm']
+accounts/reset/done/ [name='password_reset_complete']
+
+'''
--- demo/settings.py
+++ demo/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from .config import MY_EMAIL, MY_EMAIL_PASSWORD
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -129,3 +130,12 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+
+# SMTP configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = MY_EMAIL  # your email
+EMAIL_HOST_PASSWORD = MY_EMAIL_PASSWORD  # your email password
